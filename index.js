160d587c1084bcbe0a5c7b855d555734a1472b92
unknown <patilrutik501@gmail.com>
2023-07-27 13:53:07 +0530

    test

--- index.js
+++ index.js
@@ -7,66 +7,75 @@ app.use(cors())
 const swaggerUI = require('swagger-ui-express');
 const YAML = require("yamljs");
 const path = require("path");
+//importing routes
 const { productRouter } = require('./src/Routes/product.routes');
 const { cartRouter } = require('./src/Routes/cart.routes');
 const { orderRouter } = require('./src/Routes/order.routes');
 const bodyParser = require('body-parser');
 const { categoryRouter } = require('./src/Routes/category.routes');
 const { stripeRouter } = require('./src/Routes/stripe.routes');
-const {userRouter} = require('./src/Routes/user.routes')
+const { userRouter } = require('./src/Routes/user.routes')
 
 const connectDB = require("./src/config/DB.js");
-const port  = 5001
+const port = 5001
 
-app.use('/stripe/webhooks',bodyParser.raw({ type: '*/*' }));
+app.use('/stripe/webhooks', bodyParser.raw({ type: '*/*' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 
 
 
-
+//ejs config
 app.set("views", path.join("views"));
 app.set("view engine", "ejs");
 
-const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml')); 
+//swagger config
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
 app.use('/sgr', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(express.json())
-app.use('/stripe',stripeRouter)
+app.use('/stripe', stripeRouter)
 
-app.get('/',(req,res)=>{
-
+app.get('/', (req, res) => {
     res.status(200).send('Welcome homepage')
 })
 
-app.get('/test',(req,res)=>{
-    res.render("404", {
-    title: "404",
-    name: "Green Cart",
-    errorMessage: "Test",
-  });
+app.get('/test', (req, res) => {
+    res.status(200).send('test page')
 })
 
-app.get("*", (req, res) => {   
+app.get("/error", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "Green Cart",
+        errorMessage: "Page Not Found",
+    });
+});
 
-  });
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "Green Cart",
+        errorMessage: "Page Not Found",
+    });
+});
 
-app.use('/user',userRouter)
-app.use('/product',productRouter)
-app.use('/cart',cartRouter)
-app.use('/orders',orderRouter)
-app.use('/category',categoryRouter)
+app.use('/user', userRouter)
+app.use('/product', productRouter)
+app.use('/cart', cartRouter)
+app.use('/orders', orderRouter)
+app.use('/category', categoryRouter)
 
-app.listen(port,async () => {
-  try {
-    await connectDB()
-    console.log(port)
- 
-  } catch (error) {
-    console.log(error);
-  }
+app.listen(port, async () => {
+    try {
+        await connectDB()
+        console.log(port)
+
+    } catch (error) {
+        console.log(error);
+    }
     console.info(
-      `Server is running on port ${port}`
+        `Server is running on port ${port}`
     );
-  });
+});
 
