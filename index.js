bbfae87c975baa8d43e43c6251159b75d466396b
unknown <patilrutik501@gmail.com>
2023-07-27 13:39:13 +0530

    test

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 const http = require("http").createServer(app);
 const cors = require('cors');
 app.use(cors())
+const swaggerUI = require('swagger-ui-express');
+const YAML = require("yamljs");
+const path = require("path");
 const { productRouter } = require('./src/Routes/product.routes');
 const { cartRouter } = require('./src/Routes/cart.routes');
 const { orderRouter } = require('./src/Routes/order.routes');
@@ -11,16 +14,20 @@ const bodyParser = require('body-parser');
 const { categoryRouter } = require('./src/Routes/category.routes');
 const { stripeRouter } = require('./src/Routes/stripe.routes');
 const {userRouter} = require('./src/Routes/user.routes')
+
+const connectDB = require("./src/config/DB.js");
+const port  = 5001
+
 app.use('/stripe/webhooks',bodyParser.raw({ type: '*/*' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-const connectDB = require("./src/config/DB.js");
-const port  = 5001
-const swaggerUI = require('swagger-ui-express');
-const YAML = require("yamljs");
-const path = require("path");
 
+
+
+
+app.set("views", path.join("views"));
+app.set("view engine", "ejs");
 
 const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml')); 
 app.use('/sgr', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
@@ -28,8 +35,22 @@ app.use(express.json())
 app.use('/stripe',stripeRouter)
 
 app.get('/',(req,res)=>{
+
     res.status(200).send('Welcome homepage')
 })
+
+app.get('/test',(req,res)=>{
+    res.render("404", {
+    title: "404",
+    name: "Green Cart",
+    errorMessage: "Test",
+  });
+})
+
+app.get("*", (req, res) => {   
+
+  });
+
 app.use('/user',userRouter)
 app.use('/product',productRouter)
 app.use('/cart',cartRouter)
